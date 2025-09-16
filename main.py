17409ad2c9113ff1f88ed94df84fdc9da1a55351
RaihanKabir277 <raihank0198@gmail.com>
2025-09-16 13:10:54 +0600

    barear authentication added here and environment set up here for security

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #  ---------- day - 38 ----------------
 import requests
+from datetime import datetime
+# from requests.auth import HTTPBasicAuth
+import os
 
-GENDER = "male"
-WEIGHT = 72
-HEIGHT = 172
-AGE = 24
+GENDER = "YOUR GENDER"
+WEIGHT = "YOUR WEIGHT"
+HEIGHT = "YOUR HEIGHT"
+AGE = "YOUR AGE"
 
-APP_ID = "1e639f9e"
-APP_KEY = "b44be246d11f9a0d5a5014fa41dff229"
+APP_ID = os.environ["NT_APP_ID"]
+APP_KEY = os.environ["NT_API_KEY"]
 nutrionix_api = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_api = os.environ["sheety_api"]
 
 exercise_text = input("Tell me which exercise you did: ")
 
@@ -27,5 +31,40 @@ parameters = {
 
 exercise_response = requests.post(url=nutrionix_api, json=parameters, headers=headers)
 result = exercise_response.json()
-print(result)
+# print(result)
+
+# ------------ write in sheety --------
+today = datetime.now()
+DATE = today.strftime("%d/%m/%Y")
+TIME = today.strftime("%X")
+
+bearer_headers = {
+"Authorization": f"Bearer {os.environ['TOKEN']}"
+}
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout" : {
+            "date" : DATE,
+            "time" : TIME,
+            "exercise" : exercise["name"].title(),
+            "duration" : exercise["duration_min"],
+            "calories" : exercise["nf_calories"],
+        }
+    }
+
+# sheet_response = requests.post(url=sheety_api, json=sheet_inputs)
+# print(sheet_response.text)
+
+#  ------------ Authentication ---------
+#  ------ basic authentication ---------
+# sheet_response = requests.post(url=sheety_api, json=sheet_inputs, auth=("username", "pass"))
+
+# ------- Bearer authentication -----
+
+    sheet_response = requests.post(sheety_api, json=sheet_inputs, headers=bearer_headers)
+
+    print(sheet_response.text)
+
+
 
